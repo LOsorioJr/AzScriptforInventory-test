92bbf0d343849f4625ea5833f57259f8fbfecae5
LOsorioJr <48868242+LOsorioJr@users.noreply.github.com>
2023-07-26 16:29:46 -0400

    Update postSQLSingleServerInventory-2.py

--- postSQLSingleServerInventory-2.py
+++ postSQLSingleServerInventory-2.py
@@ -1,4 +1,7 @@
 import csv
+import logging
+import sys
+import argparse
 from typing import List, Tuple, Dict, Any
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resourcegraph import ResourceGraphClient
@@ -7,13 +10,23 @@ import pandas as pd
 
 
 def get_subscription_id(sub_client, subscription_name: str) -> str:
-    subscription = list(
-        sub_client.subscriptions.list(
-            filter=f"displayName eq '{subscription_name}'"))
-    return subscription[0].subscription_id if subscription else None
+    """
+    Retrieves the subscription ID for a given subscription name.
+    """
+    try:
+        subscription = list(
+            sub_client.subscriptions.list(
+                filter=f"displayName eq '{subscription_name}'"))
+        return subscription[0].subscription_id if subscription else None
+    except Exception as e:
+        logging.error(f"Error getting subscription ID for {subscription_name}: {e}")
+        return None
 
 
 def is_valid_guid(subscription_id: str) -> bool:
+    """
+    Checks if a given subscription ID is a valid GUID (Globally Unique Identifier).
+    """
     import re
     regex = re.compile(
         r'^[{\(]?[0-9a-fA-F]{8}-?[0-9a-fA-F]{4}-?[1-5][0-9a-fA-F]{3}'
@@ -22,16 +35,11 @@ def is_valid_guid(subscription_id: str) -> bool:
     return bool(regex.match(subscription_id))
 
 
-def save_not_found_subscriptions(subscriptions: List[str]) -> None:
-    with open('not_found_subscriptions.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(["SubscriptionName"])
-        for sub in subscriptions:
-            writer.writerow([sub])
-
-
-def save_invalid_subscriptions(subscriptions: List[str]) -> None:
-    with open('invalid_subscriptions.csv', 'w', newline='') as f:
+def save_subscriptions(filename: str, subscriptions: List[str]) -> None:
+    """
+    Saves a list of subscription names to a CSV file.
+    """
+    with open(filename, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(["SubscriptionName"])
         for sub in subscriptions:
@@ -40,6 +48,9 @@ def save_invalid_subscriptions(subscriptions: List[str]) -> None:
 
 def get_subscription_ids(batch: List[Dict[str, str]], sub_client) -> Tuple[
         List[Tuple[str, str]], List[str], List[str]]:
+    """
+    Retrieves subscription IDs for a batch of subscription names.
+    """
     subscription_ids = []
     not_found_subs = []
     invalid_subs = []
@@ -53,17 +64,23 @@ def get_subscription_ids(batch: List[Dict[str, str]], sub_client) -> Tuple[
         else:
             not_found_subs.append(row['SubscriptionName'])
 
-    print(f"Subscription Ids: {subscription_ids}")  # Debugging print
+    logging.info(f"Subscription Ids: {subscription_ids}")
     return subscription_ids, not_found_subs, invalid_subs
 
 
 def prepare_batches(lst: List[Any], n: int) -> List[List[Any]]:
+    """
+    Splits a list into batches of size `n`.
+    """
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
 
 def get_postgresql_servers(client, subscription_id: str,
                            resource_group: str = None) -> Any:
+    """
+    Retrieves PostgreSQL servers for a given subscription ID and optionally a resource group.
+    """
     query = ("Resources | where type =~ 'Microsoft.DBforPostgreSQL/servers' |"
              " project subscriptionId, name, resourceGroup, location, sku")
     if resource_group:
@@ -72,41 +89,44 @@ def get_postgresql_servers(client, subscription_id: str,
     return client.resources(query, [subscription_id])
 
 
-def main():
-    df = pd.read_csv("subscriptions.csv")
-    subscription_data = df[['SubscriptionName', 'ResourceGroup']].to_dict(
-        'records')
+def main(input_file: str, batch_size: int, not_found_file: str, invalid_file: str):
+    """
+    Main function that orchestrates the entire process.
+    """
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
 
-    credential = DefaultAzureCredential()
-    resource_graph_client = ResourceGraphClient(credential)
-    sub_client = SubscriptionClient(credential)
+    try:
+        df = pd.read_csv(input_file)
+    except FileNotFoundError:
+        logging.error(f"File {input_file} not found.")
+        sys.exit(1)
 
-    batch_size = 500
+    subscription_data = df[['SubscriptionName', 'ResourceGroup']].to_dict('records')
+
+    try:
+        credential = DefaultAzureCredential()
+        resource_graph_client = ResourceGraphClient(credential)
+        sub_client = SubscriptionClient(credential)
+    except Exception as e:
+        logging.error(f"Error initializing Azure clients: {e}")
+        sys.exit(1)
+
     output = []
     for batch in prepare_batches(subscription_data, batch_size):
         subscription_ids, not_found_subs, invalid_subs = get_subscription_ids(
             batch, sub_client)
-        print(f"Batch Result: {subscription_ids}, {not_found_subs},"
-              f" {invalid_subs}")  # Debugging print
-
-        save_not_found_subscriptions(not_found_subs)
-        save_invalid_subscriptions(invalid_subs)
-        for sub_id, resource_group in subscription_ids:
-            print(f"Processing: {sub_id}, {resource_group}")  # Debugging print
-            result = get_postgresql_servers(resource_graph_client, sub_id,
-                                            resource_group)
-            for row in result.data:
-                output.append({
-                    'SubscriptionId': row['subscriptionId'],
-                    'ServerName': row['name'],
-                    'ResourceGroup': row['resourceGroup'],
-                    'Location': row['location'],
-                    'SKU': row['sku']
-                })
-
-    df_output = pd.DataFrame(output)
-    df_output.to_csv("output.csv", index=False)
+        save_subscriptions(not_found_file, not_found_subs)
+        save_subscriptions(invalid_file, invalid_subs)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Process Azure subscriptions.')
+    parser.add_argument('--input', help='Input CSV file', default="subscriptions.csv")
+    parser.add_argument('--batch_size', type=int, help='Batch size', default=500)
+    parser.add_argument('--not_found_file', help='Output file for not found subscriptions', default='not_found_subscriptions.csv')
+    parser.add_argument('--invalid_file', help='Output file for invalid subscriptions', default='invalid_subscriptions.csv')
+
+    args = parser.parse_args()
+
+    main(args.input, args.batch_size, args.not_found_file, args.invalid_file)
