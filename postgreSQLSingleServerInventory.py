bdc57f1b0fc5aebd29222921f8f7e89164f40ed7
LOsorioJr <48868242+LOsorioJr@users.noreply.github.com>
2023-07-27 13:46:46 -0400

    Update postgreSQLSingleServerInventory.py

--- postgreSQLSingleServerInventory.py
+++ postgreSQLSingleServerInventory.py
@@ -1,92 +1,66 @@
-import csv
+import argparse
+import logging
+import pandas as pd
 from typing import List, Tuple, Dict, Any
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resourcegraph import ResourceGraphClient
 from azure.mgmt.subscription import SubscriptionClient
-import pandas as pd
-import logging
 
-
-def get_subscription_id(sub_client, subscription_name: str) -> str:
-    subscription = list(
-        sub_client.subscriptions.list(
-            filter=f"displayName eq '{subscription_name}'"))
-    return subscription[0].subscription_id if subscription else None
-
-
-def is_valid_guid(subscription_id: str) -> bool:
-    import re
-    regex = re.compile(
-        r'^[{\(]?[0-9a-fA-F]{8}-?[0-9a-fA-F]{4}-?[1-5][0-9a-fA-F]{3}'
-        r'-?[89abAB][0-9a-fA-F]{3}-?[0-9a-fA-F]{12}[}\)]?$'
-    )
-    return bool(regex.match(subscription_id))
-
-
-def save_not_found_subscriptions(subscriptions: List[str]) -> None:
-    with open('not_found_subscriptions.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(["SubscriptionName"])
-        for sub in subscriptions:
-            writer.writerow([sub])
-
-
-def save_invalid_subscriptions(subscriptions: List[str]) -> None:
-    with open('invalid_subscriptions.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(["SubscriptionName"])
-        for sub in subscriptions:
-            writer.writerow([sub])
-
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 def get_subscription_ids(batch: List[Dict[str, str]], sub_client) -> Tuple[
-        List[Tuple[str, str]], List[str], List[str]]:
+    List[Tuple[str, str]], List[str], List[str]]:
     subscription_ids = []
     not_found_subs = []
     invalid_subs = []
     for row in batch:
         sub_id = get_subscription_id(sub_client, row['SubscriptionName'])
-        if sub_id is not None:
-            if is_valid_guid(sub_id):
-                subscription_ids.append((sub_id, row['ResourceGroup']))
-            else:
-                invalid_subs.append(row['SubscriptionName'])
-        else:
+        if sub_id is None:
             not_found_subs.append(row['SubscriptionName'])
-
-    logging.info(f"Subscription Ids: {subscription_ids}")
+        elif not is_valid_guid(sub_id):
+            invalid_subs.append(row['SubscriptionName'])
+        else:
+            subscription_ids.append((sub_id, row['ResourceGroupName']))
     return subscription_ids, not_found_subs, invalid_subs
 
-
-def prepare_batches(lst: List[Any], n: int) -> List[List[Any]]:
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
-
-
-def get_postgresql_servers(client, subscription_id: str,
-                           resource_group: str = None) -> Any:
+def get_postgresql_servers(client, subscription_id: str) -> Any:
+    """
+    Retrieves PostgreSQL servers for a given subscription ID.
+    """
     query = ("Resources | where type =~ 'Microsoft.DBforPostgreSQL/servers' |"
              " project subscriptionId, name, resourceGroup, location, sku")
-    if resource_group:
-        query += f"| where resourceGroup == '{resource_group}'"
-
     return client.resources(query, [subscription_id])
 
-
-def main():
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-
-    df = pd.read_csv("subscriptions.csv")
-    subscription_data = df[['SubscriptionName', 'ResourceGroup']].to_dict(
-        'records')
+def main(args: argparse.Namespace) -> None:
+    df = pd.read_csv(args.input_file)
+    subscription_data = df[['SubscriptionName', 'ResourceGroupName']].to_dict('records')
 
     credential = DefaultAzureCredential()
     resource_graph_client = ResourceGraphClient(credential)
     sub_client = SubscriptionClient(credential)
 
-    batch_size = 500
     output = []
-    for batch in prepare_batches(subscription_data, batch_size):
+    for batch in prepare_batches(subscription_data, args.batch_size):
         subscription_ids, not_found_subs, invalid_subs = get_subscription_ids(
-            batch, sub_
+            batch, sub_client)
+        save_subscriptions(args.not_found_file, not_found_subs)
+        save_subscriptions(args.invalid_file, invalid_subs)
+
+        # Use the resource_graph_client to get PostgreSQL servers for each subscription ID
+        for sub_id, resource_group in subscription_ids:
+            servers = get_postgresql_servers(resource_graph_client, sub_id,
+                                             resource_group)
+            output.append(servers)
+    print(output)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process Azure subscriptions.')
+    parser.add_argument('--input', help='Input CSV file', default="subscriptions.csv")
+    parser.add_argument('--batch_size', type=int, help='Batch size', default=500)
+    parser.add_argument('--not_found_file', help='Output file for not found subscriptions', default='not_found_subscriptions.csv')
+    parser.add_argument('--invalid_file', help='Output file for invalid subscriptions', default='invalid_subscriptions.csv')
+
+    args = parser.parse_args()
+
+    main(args)
