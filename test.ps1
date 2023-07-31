e7875afef6df0c5bf87d60be2504a29fed98080c
LOsorioJr <48868242+LOsorioJr@users.noreply.github.com>
2023-07-31 10:05:56 -0400

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -2,13 +2,19 @@ import csv
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.rdbms import postgresql
+from azure.mgmt.subscription import SubscriptionClient
+from tqdm import tqdm
 
 def get_credentials():
-    # This will use the default credentials from your local machine
     return DefaultAzureCredential()
 
-def get_resource_client(credentials, subscription_id):
-    return ResourceManagementClient(credentials, subscription_id)
+def get_subscription_id(credentials, subscription_name):
+    subscription_client = SubscriptionClient(credentials)
+    subscriptions = subscription_client.subscriptions.list()
+    for subscription in subscriptions:
+        if subscription.display_name == subscription_name:
+            return subscription.subscription_id
+    return None
 
 def get_postgresql_client(credentials, subscription_id):
     return postgresql.PostgreSQLManagementClient(credentials, subscription_id)
@@ -38,19 +44,36 @@ def write_to_csv(servers, file_path):
                 'SKU': server.sku.name
             })
 
+def write_not_found_to_csv(not_found, file_path):
+    with open(file_path, 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=['SubscriptionName', 'ResourceGroupName'])
+        writer.writeheader()
+        writer.writerows(not_found)
+
 def main():
     credentials = get_credentials()
     subscriptions_and_resource_groups = get_subscriptions_and_resource_groups_from_csv('subscriptions.csv')
     servers = []
+    not_found = []
 
-    for item in subscriptions_and_resource_groups:
-        subscription_id = item['SubscriptionName']
+    for item in tqdm(subscriptions_and_resource_groups, desc="Processing subscriptions"):
+        subscription_name = item['SubscriptionName']
+        subscription_id = get_subscription_id(credentials, subscription_name)
+        if subscription_id is None:
+            not_found.append(item)
+            continue
+
         resource_group = item.get('ResourceGroupName')
 
-        postgresql_client = get_postgresql_client(credentials, subscription_id)
-        servers.extend(get_postgresql_servers(postgresql_client, resource_group))
+        try:
+            postgresql_client = get_postgresql_client(credentials, subscription_id)
+            servers.extend(get_postgresql_servers(postgresql_client, resource_group))
+        except Exception as e:
+            print(f"Error while processing subscription {subscription_name} and resource group {resource_group}: {e}")
+            not_found.append(item)
 
     write_to_csv(servers, 'output.csv')
+    write_not_found_to_csv(not_found, 'not_found.csv')
 
 if __name__ == "__main__":
     main()
